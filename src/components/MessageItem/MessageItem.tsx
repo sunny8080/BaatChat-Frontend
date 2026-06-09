d437bb46d0be8ec148cd5a1f1df98738a43c7139
Sunny Kumar <sk7522422@email.com>
2026-06-10 01:34:15 +0530

    1006- add notification capsules

--- src/components/MessageItem/MessageItem.tsx
+++ src/components/MessageItem/MessageItem.tsx
@@ -5,7 +5,7 @@ import { formatMsgTime } from '../../utils/utils';
 import './MessageItem.scss';
 import { useChatDetailsStore } from '../../zustand/ChatDetailsStore';
 import type { ChatDetailsInterface } from '../../interfaces/ChatDetailsInterface';
-import { ChatTypes } from '../../utils/constant';
+import { ChatTypes, MessageTypes } from '../../utils/constant';
 
 type Props = {
   msg: MessageInterface;
@@ -14,35 +14,48 @@ type Props = {
 const MessageItem = ({ msg }: Props) => {
   const { user } = useAuth();
   const isCurrentUserIsSender = msg.sender?.id === user?.id;
+  const isNotification = msg.type === MessageTypes.NOTIFICATION;
   const chatDetails: ChatDetailsInterface | null = useChatDetailsStore(
     (state) => state.chatDetails,
   );
 
   return (
-    <div className={`bc-MessageItem ${isCurrentUserIsSender ? 'msgOut' : 'msgIn'}`}>
-      <div className="bc-msg-content-wrapper">
-        {chatDetails?.type === ChatTypes.GROUP && !isCurrentUserIsSender && (
-          <div className="bc-msg-avatar">
-            <img src={msg.sender?.avatarUrl} alt={msg.sender?.name} />
-          </div>
-        )}
-
-        {/* TODO - how we will handle for files  */}
-        <div className="bc-msg-content">{msg.text}</div>
-
-        <div className="bc-msg-reaction-trigger">
-          <Smile size={12} color="white" />+
+    <div
+      className={`bc-MessageItem ${isNotification ? 'notification' : isCurrentUserIsSender ? 'msgOut' : 'msgIn'}`}
+    >
+      {isNotification && (
+        <div className="bc-msg-content-wrapper">
+          <div className="bc-msg-content">{msg.text}</div>
         </div>
-      </div>
+      )}
 
-      <div className="bc-msg-time">
-        {formatMsgTime(msg.createdAt!)}
-        {isCurrentUserIsSender && <span className="msg-sent-check">✓✓</span>}
-      </div>
+      {!isNotification && (
+        <>
+          <div className="bc-msg-content-wrapper">
+            {chatDetails?.type === ChatTypes.GROUP && !isCurrentUserIsSender && (
+              <div className="bc-msg-avatar">
+                <img src={msg.sender?.avatarUrl} alt={msg.sender?.name} />
+              </div>
+            )}
 
-      <div className="bc-msg-reactions-container"></div>
+            {/* TODO - how we will handle for files  */}
+            <div className="bc-msg-content">{msg.text}</div>
 
-      <div className="bc-msg-reaction-picker"></div>
+            <div className="bc-msg-reaction-trigger">
+              <Smile size={12} color="white" />+
+            </div>
+          </div>
+
+          <div className="bc-msg-time">
+            {formatMsgTime(msg.createdAt!)}
+            {isCurrentUserIsSender && <span className="msg-sent-check">✓✓</span>}
+          </div>
+
+          <div className="bc-msg-reactions-container"></div>
+
+          <div className="bc-msg-reaction-picker"></div>
+        </>
+      )}
     </div>
   );
 };
